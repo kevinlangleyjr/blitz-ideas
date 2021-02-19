6567b6033d92e5c34b5c0390c3a6db67b914a3e2
Kevin Langley <me@kevinlangleyjr.com>
2021-02-18 20:55:55 -0500

    Adding LabeledTextareaField component

---
+++ app/core/components/LabeledTextareaField.tsx
@@ -0,0 +1,57 @@
+import { forwardRef, PropsWithoutRef } from 'react';
+import { useField } from 'react-final-form';
+
+export interface LabeledTextareaFieldProps extends PropsWithoutRef<JSX.IntrinsicElements['textarea']> {
+  /** Field name. */
+  name: string
+  /** Field label. */
+  label: string
+  outerProps?: PropsWithoutRef<JSX.IntrinsicElements['div']>
+}
+
+export const LabeledTextareaField = forwardRef<HTMLTextAreaElement, LabeledTextareaFieldProps>(
+  ( { name, label, outerProps, ...props }, ref ) => {
+    const {
+      input,
+      meta: { touched, error, submitError, submitting },
+    } = useField( name, {
+      parse: undefined,
+    } );
+
+    const normalizedError = Array.isArray( error ) ? error.join( ', ' ) : error || submitError;
+
+    return (
+      <div { ...outerProps }>
+        <label>
+          { label }
+          <textarea { ...input } disabled={ submitting } { ...props } ref={ ref } />
+        </label>
+
+        { touched && normalizedError && (
+          <div role="alert" style={ { color: 'red' } }>
+            { normalizedError }
+          </div>
+        ) }
+
+        <style jsx>{ `
+          label {
+            display: flex;
+            flex-direction: column;
+            align-items: start;
+            font-size: 1rem;
+          }
+          textarea {
+            font-size: 1rem;
+            padding: 0.25rem 0.5rem;
+            border-radius: 3px;
+            border: 1px solid purple;
+            appearance: none;
+            margin-top: 0.5rem;
+          }
+        ` }</style>
+      </div>
+    );
+  }
+);
+
+export default LabeledTextareaField;
