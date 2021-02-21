467aaa1341e11f5d78403fc8aa73e8f67c689980
Kevin Langley <me@kevinlangleyjr.com>
2021-02-21 16:18:02 -0500

    Invalidate getCurrentUser query when user completes signup process

--- app/auth/pages/signup.tsx
+++ app/auth/pages/signup.tsx
@@ -1,13 +1,17 @@
-import { useRouter, BlitzPage } from 'blitz';
+import { invalidateQuery, useRouter, BlitzPage } from 'blitz';
 import Layout from 'app/core/layouts/Layout';
 import { SignupForm } from 'app/auth/components/SignupForm';
+import getCurrentUser from 'app/users/queries/getCurrentUser';
 
 const SignupPage: BlitzPage = () => {
   const router = useRouter();
 
   return (
     <div>
-      <SignupForm onSuccess={ () => router.push( '/' ) } />
+      <SignupForm onSuccess={ () => {
+        router.push( '/' );
+        invalidateQuery( getCurrentUser );
+      } } />
     </div>
   );
 };
