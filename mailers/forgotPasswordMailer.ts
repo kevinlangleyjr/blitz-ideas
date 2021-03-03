a52e36fffebbb5b97db69c1a9596d57db54b6333
Kevin Langley <me@kevinlangleyjr.com>
2021-03-03 04:51:30 -0500

    Fixing reference to mail integration

--- mailers/forgotPasswordMailer.ts
+++ mailers/forgotPasswordMailer.ts
@@ -4,7 +4,7 @@
  * and then export it. That way you can import here and anywhere else
  * and use it straight away.
  */
-import { mail } from 'mailers/mail';
+import { mail } from 'integrations/mail';
 
 type ResetPasswordMailer = {
   to: string
