d80b66becece55742f12d79d9dab7f2b64bb8195
Kevin Langley <me@kevinlangleyjr.com>
2021-03-04 16:54:49 -0500

    Finishing the LabelTextareaField and adding classes when using it in the IdeaForm

--- app/core/components/LabeledTextareaField.tsx
+++ app/core/components/LabeledTextareaField.tsx
@@ -4,35 +4,63 @@ import { useField } from 'react-final-form';
 export interface LabeledTextareaFieldProps extends PropsWithoutRef<JSX.IntrinsicElements['textarea']> {
   /** Field name. */
   name: string
+  /** Field inner container props. */
+  innerProps?: PropsWithoutRef<JSX.IntrinsicElements['div']>
   /** Field label. */
   label: string
+  /** Field label props. */
+  labelProps?: PropsWithoutRef<JSX.IntrinsicElements['label']>
+  /** Field type. Doesn't include radio buttons and checkboxes */
+  type?: 'text' | 'password' | 'email' | 'number'
+  /** Field outer container props. */
   outerProps?: PropsWithoutRef<JSX.IntrinsicElements['div']>
 }
 
 export const LabeledTextareaField = forwardRef<HTMLTextAreaElement, LabeledTextareaFieldProps>(
-  ( { name, label, outerProps, ...props }, ref ) => {
+  ( {
+    name,
+    innerProps,
+    label,
+    labelProps,
+    outerProps,
+    ...props
+  }, ref ) => {
     const {
       input,
       meta: { touched, error, submitError, submitting },
-    } = useField( name, {
-      parse: undefined,
-    } );
+    } = useField( name );
 
     const normalizedError = Array.isArray( error ) ? error.join( ', ' ) : error || submitError;
 
     return (
-      <div { ...outerProps }>
-        <label>
-          { label }
-          <textarea { ...input } disabled={ submitting } { ...props } ref={ ref } rows={ 20 } cols={ 60 } />
-        </label>
-
+      <>
+        <div { ...outerProps }>
+          <label { ...labelProps }>
+            { label }
+          </label>
+          <div { ...innerProps }>
+            <textarea rows={ 3 } { ...input } disabled={ submitting } { ...props } ref={ ref } />
+          </div>
+        </div>
         { touched && normalizedError && (
-          <div role="alert">
-            { normalizedError }
+          <div className="rounded-md bg-red-50 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <svg className="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                </svg>
+              </div>
+              <div className="ml-3">
+                <div className="text-sm text-red-700">
+                  <p>
+                    { normalizedError }
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         ) }
-      </div>
+      </>
     );
   }
 );
--- app/ideas/components/IdeaForm.tsx
+++ app/ideas/components/IdeaForm.tsx
@@ -8,7 +8,12 @@ export function IdeaForm<S extends z.ZodType<any, any>>( props: FormProps<S> ) {
   return (
     <Form<S> { ...props }>
       <LabeledTextField name="title" label="Title" placeholder="Title" />
-      <LabeledTextareaField name="body" label="Content" placeholder="Content" />
+      <LabeledTextareaField
+        name="body"
+        label="Content"
+        placeholder="Content"
+        className="block w-full shadow-sm focus:ring-light-blue-500 focus:border-light-blue-500 sm:text-sm border-gray-300 rounded-md"
+      />
     </Form>
   );
 }
